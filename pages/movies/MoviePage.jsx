0d9c631a45b9b04bd8565e1527855d78a50d8933
Vincenzo Iuliano <iuliano-vincenzo@live.it>
2025-01-13 18:20:18 +0100

    ADD: new revie form

--- pages/movies/MoviePage.jsx
+++ pages/movies/MoviePage.jsx
@@ -51,6 +51,40 @@ function MoviePage() {
             <ReviewCard review={review} key={review.id} />
           ))}
         </ul>
+        <form className="container mb-3">
+          <div className="m-2">
+            <label for="Nome">Nome</label>
+            <input
+              type="text"
+              class="form-control"
+              id="name"
+              placeholder="Inserisci il tuo nome"
+            ></input>
+          </div>
+          <div className="m-2">
+            <label for="Testo">Testo della recensione</label>
+            <input
+              type="text"
+              class="form-control"
+              name="text"
+              placeholder="Inserisci qui il testo della tua recensione"
+            />
+          </div>
+          <div className="m-2">
+            <label for="Voto">Voto della tua recensione: </label>
+            <select class="custom-select">
+              <option selected>Scegli il voto che vuoi dare alla tua recensione</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </div>
+          <div>
+            <button type="submit" className="rounded p-2 m-2">Invia la tua recensione!</button>
+          </div>
+        </form>
       </>
     ) : (
       <div>Nessun film presente in archivio</div>
